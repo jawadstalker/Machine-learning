2ad671fea05dcdac38434308953690673dcaea53
Death stalker <133969331+jawadstalker@users.noreply.github.com>
2026-07-20 23:58:46 +0330

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 df = pd.read_csv("magic04.data", names = cols)
 print(df.head())
